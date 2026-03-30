df4ec9eca8149bcdaac65fb8ed1af29bd4d3936a
Blake Nelson <blake_0309@aol.com>
2026-03-30 18:37:25 -0500

    Commit

--- HW3Config.py
+++ HW3Config.py
@@ -1,12 +1,16 @@
-from TextGenRNN import generate_text
-from WikiTextLSTM import generate_text_wiki
+# from TextGenRNN import generate_text
+# from WikiTextLSTM import generate_text_wiki
 from LSTMTranslate import run
 from RNNTranslate import runRNN
+import tensorflow as tf
 
-print(generate_text(seed="ROMEO:\n", length=500))
+# model = tf.keras.models.load_model('shakespeare_rnn_tf.weights.h5')
+# print(generate_text(model, seed="ROMEO:\n", length=500)) # loss: 1.7888  |  perplexity: 5.98
 
-print(generate_text_wiki(seed="the history of science", length=100))
+# print(generate_text_wiki(seed="the history of science", length=100)) 
 
-run()
-
+run() # loss: 3.6690 - perplexity: 40.6524
+# ── BLEU scores (greedy, 500 samples) ──
+# [train]  Corpus BLEU-4: 11.97
+# [val  ]  Corpus BLEU-4: 13.56
 runRNN()
